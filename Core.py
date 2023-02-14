f08924f23a95974bf55f84a6199918129953c52c
karakaron <karakaron@gmail.com>
2023-02-14 20:43:27 +0100

    Sampling is separated from pid step -> pid can have higher frequency. Fixed indexing error of vehicles in platoons.

--- Core.py
+++ Core.py
@@ -12,19 +12,29 @@ class Simulation(carla.Client):
 		self.map = self.world.get_map()
 		self.spectator = self.world.get_spectator()
 
-		if not render:
-			_settings = self.world.get_settings()  # turn rendering off
-			_settings.no_rendering_mode = True
-			self.world.apply_settings(_settings)
+		_settings = self.world.get_settings()
+		_settings.no_rendering_mode = not render
+		# _settings.fixed_delta_seconds = 0.01
+		# _settings.substepping = True
+		# _settings.max_substep_delta_time = 0.01
+		# _settings.max_substeps = 10
+		# _settings.synchronous_mode = True
+		if world == 'Town11':
+			_settings.actor_active_distance = 2000
+		self.world.apply_settings(_settings)
 
 		self.platoons = []
 
 	def add_platoon(self, platoon):
 		self.platoons.append(platoon)
 
-	def run_step(self):
+	def run_step(self, mode="control"):
+		# self.world.tick()
+		# mode: either "control" or "sample"; determines if new target speed is computed or only pid iteration is called
 		for platoon in self.platoons:
-			platoon.run_step()
+			if mode == "sample":
+				platoon.take_measurements()
+			platoon.run_pid_step()
 
 	def run(self):
 		pass
@@ -43,31 +53,39 @@ class Platoon:
 		if item == 0:
 			return self.lead_vehicle
 		elif isinstance(item, int):
-			return self.follower_vehicles[item]
+			return self.follower_vehicles[item-1]
 		else:
 			raise IndexError("Index must be an integer.")
 
 	def add_lead_vehicle(self, blueprint, spawn_point):
 		if self.lead_vehicle is None:
 			self.lead_vehicle = Vehicle(blueprint, spawn_point, self.world, 0)
-			self.lead_vehicle.attach_controller(local_planner.LocalPlanner(self.lead_vehicle))
+			self.lead_vehicle.attach_controller(local_planner.LocalPlanner(self.lead_vehicle,
+							{"longitudinal_control_dict": {'K_P': 2/5, 'K_I': 0.26/2, 'K_D': 0.26/3, 'dt': 0.01}}))
 			return self.lead_vehicle
 		else:
 			raise Exception("This platoon already has a lead vehicle.")
 
-	def add_follower_vehicle(self, blueprint, spawn_point, index = None):  # index: 0 is the lead vehicle
+	def add_follower_vehicle(self, blueprint, spawn_point, index=None):  # index: 0 is the lead vehicle
 		if index is None:
 			index = len(self.follower_vehicles)
 		_new_vehicle = Vehicle(blueprint, spawn_point, self.world, index)
-		self.follower_vehicles.insert(index-1, _new_vehicle)
+		self.follower_vehicles.insert(index, _new_vehicle)
 		for i, vehicle in enumerate(self.follower_vehicles):	 # adjust indices
 			vehicle.index = i + 1
 		return _new_vehicle
 
-	def run_step(self):
+	def take_measurements(self):
+		self.lead_vehicle.controller.set_speed(90)  # todo: allow custom control of lead vehicle
 		self.lead_waypoints.append(self.map.get_waypoint(self.lead_vehicle.get_location()))
-		self.lead_vehicle.controller.set_speed(30)		# todo: allow custom control of lead vehicle
-		self.lead_vehicle.apply_control(self.lead_vehicle.controller.run_step(debug=True))
+		for vehicle in self.follower_vehicles:
+			vehicle.controller.compute_target_speed(vehicle.index)
+
+	def run_pid_step(self):
+		try:
+			self.lead_vehicle.apply_control(self.lead_vehicle.controller.run_step())
+		except Exception as e:
+			print(e)
 		for vehicle in self.follower_vehicles:
 			vehicle.control(lead_waypoints=self.lead_waypoints)
 
@@ -92,6 +110,7 @@ class Vehicle:
 		self.world = world
 		self._carla_vehicle = world.spawn_actor(blueprint, spawn_point)
 		self.index = index
+		self.controller = None
 
 	def attach_controller(self, controller):
 		self.controller = controller
@@ -110,24 +129,27 @@ class FollowerController:
 	"""Controller for follower vehicles. Desired speed is given by a user-defined function at every time step.
 	Spatial trajectory is inherited from the lead vehicle"""
 	def __init__(self, vehicle, control_function, parameters, platoon):
-		# parameters: ORDERED list of 2(or more)-tuples of vehicle index and attribute(s), 0 is ego
+		# parameters: ORDERED list of 2(or more)-tuples of vehicle index and attribute(s), index 0 is ego
 		self.control_function = control_function
 		self.parameters = parameters
 		self.vehicle = vehicle
 		self.pid = controller.VehiclePIDController(self.vehicle,
-												   args_lateral={'K_P': 1, 'K_I': 0.05, 'K_D': 0.2, 'dt': 0.05},
-												   args_longitudinal={'K_P': 3/5, 'K_I': 0.29/2, 'K_D': 0.29/3, 'dt': 0.05})
+												   args_lateral={'K_P': 1.95, 'K_I': 0.05, 'K_D': 0.2, 'dt': 0.01},
+												   args_longitudinal={'K_P': 2/5, 'K_I': 0.26/2, 'K_D': 0.26/3, 'dt': 0.01},
+												   max_brake=0.3, max_throttle=0.75)
 		self.platoon = platoon
+		self.target_speed = 0
+
+	def compute_target_speed(self, index):
+		_args = [getattr(self.platoon[index + p[0]], *p[1:]) for p in self.parameters]
+		self.target_speed = self.control_function(*_args)
 
 	def compute_control(self, lead_waypoints, index):  # index is ego vehicle index
-		# compute target speed via user defined control strategy
-		_args = [getattr(self.platoon[index + p[0]], *p[1:]) for p in self.parameters]
-		target_speed = self.control_function(_args)
 		# find next waypoint
 		_to_remove = 0
 		for i, wp in enumerate(lead_waypoints):
 			if self.vehicle.get_location().distance(wp.transform.location) < 2:
 				_to_remove = max(_to_remove, i + 1)  # warning: this causes problems in a loop-like trajectory
 		del lead_waypoints[0:_to_remove]
-		return self.pid.run_step(target_speed, lead_waypoints[0])
+		return self.pid.run_step(self.target_speed, lead_waypoints[0])
 
